7b48e18927e27abd1b63f8712ec4084f4c88caa8
Syed Farhan Ali Zaidi <101693261+farhanxaidi@users.noreply.github.com>
2025-02-17 22:20:31 +0500

    Update index.js

--- pages/about/index.js
+++ pages/about/index.js
@@ -23,21 +23,21 @@ const aboutData = [
       {
         title: "Web Development",
         icons: [
-          { icon: <FaHtml5 />, key: "html5" },
-          { icon: <FaCss3 />, key: "css3" },
-          { icon: <FaJs />, key: "js" },
-          { icon: <FaReact />, key: "react" },
-          { icon: <SiNextdotjs />, key: "nextjs" },
-          { icon: <SiFramer />, key: "framer" },
-          { icon: <FaWordpress />, key: "wordpress" },
+          { icon: FaHtml5, key: "html5" },
+          { icon: FaCss3, key: "css3" },
+          { icon: FaJs, key: "js" },
+          { icon: FaReact, key: "react" },
+          { icon: SiNextdotjs, key: "nextjs" },
+          { icon: SiFramer, key: "framer" },
+          { icon: FaWordpress, key: "wordpress" },
         ],
       },
       {
         title: "UI/UX Design",
         icons: [
-          { icon: <FaFigma />, key: "figma" },
-          { icon: <SiAdobexd />, key: "adobexd" },
-          { icon: <SiAdobephotoshop />, key: "photoshop" },
+          { icon: FaFigma, key: "figma" },
+          { icon: SiAdobexd, key: "adobexd" },
+          { icon: SiAdobephotoshop, key: "photoshop" },
         ],
       },
     ],
@@ -132,7 +132,7 @@ const About = () => {
             className="max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0"
           >
             10 years ago, I began freelancing as a developer. Since then, I&apos;ve
-            done remote work for agencies, counsulted for startups, and
+            done remote work for agencies, consulted for startups, and
             collaborated on digital products for business and consumer use.
           </motion.p>
           <motion.div
@@ -186,43 +186,17 @@ const About = () => {
           className="flex flex-col w-full xl:max-w-[48%] h-[480px]"
         >
           <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
-            {aboutData.map((item, itemIndex) => {
-              return (
-                <div
-                  key={itemIndex}
-                  className={`${
-                    index === itemIndex &&
-                    "text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300"
-                  } cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after;absolute after:-bottom-1 after:left-0`}
-                  onClick={() => setIndex(itemIndex)}
-                >
-                  {item.title}
-                </div>
-              );
-            })}
-          </div>
-          <div className="py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start">
-            {aboutData[index].info.map((item, itemIndex) => {
-              return (
-                <div
-                  key={itemIndex}
-                  className="flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60"
-                >
-                  <div className="font-light mb-2 md:mb-0">{item.title}</div>
-                  <div className="hidden md:flex">-</div>
-                  <div>{item.stage}</div>
-                  <div className="flex gap-x-4">
-                    {item.icons?.map((icon, itemIndex) => {
-                      return (
-                        <div key={itemIndex} className="text-2xl text-white">
-                          {icon}
-                        </div>
-                      );
-                    })}
-                  </div>
-                </div>
-              );
-            })}
+            {aboutData.map((item, itemIndex) => (
+              <div
+                key={itemIndex}
+                className={`cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0 ${
+                  index === itemIndex ? "text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300" : ""
+                }`}
+                onClick={() => setIndex(itemIndex)}
+              >
+                {item.title}
+              </div>
+            ))}
           </div>
         </motion.div>
       </div>
